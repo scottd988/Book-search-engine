00342656da81a300aee0b772e48748c5110f383c
scottd988 <>
2024-03-08 13:31:23 -0500

    login mutation and add user

--- Develop/client/src/components/LoginForm.jsx
+++ Develop/client/src/components/LoginForm.jsx
@@ -2,8 +2,9 @@
 import { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
